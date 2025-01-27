b310d514601db4e5c2640b60108a3786dfdb18d2
Martin Manjoro <martin.manjoro25@gmail.com>
2025-01-27 23:55:11 +0200

    Implement NoteCard hover effect and edit mode entry
    - Added hover effect to NoteCard for displaying Edit and Delete buttons on hover.
    - Made NoteCard clickable to enter edit mode.

--- renderer/components/NoteCard.tsx
+++ renderer/components/NoteCard.tsx
@@ -12,31 +12,33 @@ const NoteCard: React.FC<NoteCardProps> = ({ title, content, createdAt, updatedA
   };
 
   return (
-    <div className='border border-gray-300 rounded-md p-4 mb-4 shadow-sm text-white'>
-      <h3 className='text-lg font-bold'>
-        {title}
-      </h3>
-      <p className='text-gray-600 mt-2'>
-        {content}
-      </p>
+    <div
+      className="border border-gray-700 rounded-md p-4 mb-4 shadow-sm hover:border-gray-300 text-white relative group cursor-pointer transition duration-75"
+      onClick={onEdit} // Trigger edit mode on click
+    >
+      <h3 className="text-lg font-bold">{title}</h3>
+      <p className="text-gray-600 mt-2">{content}</p>
       <p className="text-sm mt-4">
         {updatedAt ? `Edited: ${formatDate(updatedAt)}` : `${formatDate(createdAt)}`}
       </p>
-      <Button
-        size='sm'
-        onClick={onDelete}
-        className="mt-4 bg-red-500 px-4 py-2 rounded-md hover:bg-red-600"
-      >
-        Delete
-      </Button>
-      <Button
-        size='sm'
-        onClick={onEdit}
-        className="mt-4 bg-yellow-500 px-4 py-2 rounded-md hover:bg-yellow-600"
-      >
-        Edit
-      </Button>
 
+      {/* Edit and Delete buttons will appear when the card is hovered */}
+      <div className="absolute top-4 right-4 opacity-0 group-hover:opacity-100 transition-opacity">
+        <Button
+          size="sm"
+          onClick={onDelete}
+          className="bg-red-500 px-4 py-2 rounded-md hover:bg-red-600"
+        >
+          Delete
+        </Button>
+        <Button
+          size="sm"
+          onClick={onEdit}
+          className="bg-yellow-500 px-4 py-2 rounded-md hover:bg-yellow-600 mt-2"
+        >
+          Edit
+        </Button>
+      </div>
     </div>
   );
 };
